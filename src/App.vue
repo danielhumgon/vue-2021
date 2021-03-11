ef46f60cc2896808f0d6080127f7420795e51473
Daniel Gonzalez <danielhumgon@gmail.com>
2021-03-11 00:01:58 -0400

    Created a Header component - passed title as props

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <h1>Hello!</h1>
+<div class="container">
+    <Header title="Task Tracker"/>
+</div>
+  
 </template>
 
 <script>
 
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
